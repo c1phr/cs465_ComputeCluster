af157c235ddd855a89dfe861ba65acf4605c111f
Andrew Stebenne <ssonicblue@gmail.com>
2014-10-01 18:10:03 -0700

    Basic defn for CentralServer
    Added self.ip_address, self.send_port, self.listen_port to
    CentralServer.__init__()

--- src/CentralServer.py
+++ src/CentralServer.py
@@ -2,4 +2,7 @@ from Connection_Info import *
 
 class CentralServer(object):
     def __init__(self):
-        pass
+        self.ip_address = self.Get_IP()
+        self.send_port = self.Get_Send_Port()
+        self.listen_port = self.Get_Listen_Port()
+
