ee9b447ffe9f9f7ec905306590937da714c22e9b
Andrew Stebenne <ssonicblue@gmail.com>
2014-10-01 18:15:37 -0700

    Added a job queue to CentralServer

--- src/CentralServer.py
+++ src/CentralServer.py
@@ -1,8 +1,11 @@
 from Connection_Info import *
+import queue.py
 
 class CentralServer(object):
     def __init__(self):
         self.ip_address = self.Get_IP()
         self.send_port = self.Get_Send_Port()
         self.listen_port = self.Get_Listen_Port()
+        self.job_queue = Queue( 10 )
+        # Job queue has a max size of 10
 
