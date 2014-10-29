7c821ef0b5c586957eae9b80756d202ae1e10564
Ryan Batchelder <rb688@nau.edu>
2014-10-28 17:10:48 -0700

    Aux processor multi-process should be implemented and the class message-ified

--- src/auxiliary_processor.py
+++ src/auxiliary_processor.py
@@ -15,62 +15,30 @@ class AuxiliaryProcessor(object):
 
     def run_file(self, file):
         module = __import__(file[:-3])
-        module.main()
+        return module.main()
 
     def process(self, data, ip):
         in_file = file_ops.bytes_to_file(data)
-        self.run_file(in_file)
-        file_arr = file_ops.file_to_bytes("out.txt")
-        #open the socket, set connections, connect to servers listen port,
-        #send process result, then close
-        self.socket_a = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket_a.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.socket_a.connect((ip, self.listen_port))
-        self.socket_a.send(file_arr)
-        self.socket_a.close()
+        out = self.run_file(in_file)
+        return_message = message.Message("r", out)
+        self.send_message(return_message)
 
-    def listening(self):
-        self.connection = Connection_Info(socket.gethostbyname(socket.gethostname()))
-        self.socket_con = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # open socket
-        self.socket_con.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.socket_con.bind((socket.gethostname(), self.connection.listening_port))
-        self.socket_con.listen(15)  # up to fifteen users can message at once. Can change later
-        self.socket_con.setblocking(False)  # opens the non blocking channel
-        print(self.ip_address)
-
-        if self.socket_con:
-            input = [self.socket_con]
-            while True:
-                input_ready, output_ready, errors = select.select(input, [], [])
-
-                for sock in input_ready:
-                    if sock is self.socket_con:
-                        client, address = sock.accept()
-                        input.append(client)
-                    else:
-                        data = sock.recv(self.connection.buffer).decode()
-                        if data:
-                            self.process(data, address[0])
-                        else:
-                            sock.close()
-                            input.remove(sock)
-
-    def connect(self, ip):
+    def connect(self):
+        """
+        Connect to a central server using the given ip
+        """
         # Connect to the central server and tell it how many threads we have
         join_message = message.Message('c', self.avail_threads)
         self.send_message(join_message)
 
-
     def send_message(self, message):
-        to_send = message.To_Json().encode()  # Serialize the data into JSON so it can
-        # be sent over the socket
-
+        # Serialize the data into JSON so it can be sent over the socket
+        to_send = message.To_Json().encode()
         self.socket_con2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # open socket
         self.socket_con2.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.socket_con2.connect((self.central_ip, self.connection.get_listening_port()))  # connect to particular ip
         self.socket_con2.send(to_send)  # send the JSON encoded message
         self.socket_con2.close()  # close the socket
-
 
     def listening(self):
         self.connection = Connection_Info(socket.gethostbyname(socket.gethostname()))
