cc044e827398f409ca1e6bf1fac844f55179efa2
cmh553 <cmh553@nau.edu>
2014-10-29 20:56:01 -0700

    Added more to the Server option

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 from auxiliary_processor import AuxiliaryProcessor
 from central_server import CentralServer
+import os
+import glob
 
+# modules = glob.glob(os.path.dirname("to_process")+"/*.py")
+# __all__ = [ os.path.basename(f)[:-3] for f in modules]
 
 def main():
 
@@ -11,17 +15,20 @@ def main():
     #if you're the server
     if input_identity == "y":
         #Fetch folder with list of job files
+        process_files = os.listdir("to_process")
+        print(process_files)
         server = CentralServer()
+        print("Server IP: " + server.ip_address)
 
-        #some stuff here
 
     #if you're not the server, you must be a satellite
     else:
-        print("Starting Satellite...\n")
+        print("Starting Satellite ...\n")
         print("Enter the IP of the server:\n")
         input_serverIP = input()
         satellite = AuxiliaryProcessor(input_serverIP)
-
-
+        print("Waiting for incoming jobs...\n")
 
     return
+
+main()
