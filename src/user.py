108063e258c774a38aa8e6317b7998f3bbeb1a73
Ryan Batchelder <rb688@nau.edu>
2014-11-02 14:08:42 -0700

    Forgot user file

---
+++ src/user.py
@@ -0,0 +1,111 @@
+import json
+# noinspection PyUnresolvedReferences
+from multiprocessing import *
+from connection_info import Connection_Info
+from file_ops import file_ops
+import socket, select, sys
+from message import Message
+
+
+class User(object):
+    def __init__(self, server_ip):
+        self.connection = Connection_Info(socket.gethostbyname(socket.gethostname()))
+        self.ip_address = self.connection.get_ip()
+        self.send_port = self.connection.get_send_port()
+        self.listen_port = self.connection.get_listening_port()
+        self.server_ip = server_ip
+
+
+    def send(self, to_send, aux_ip):
+        """
+        Sends a job to an available compute node. Jobs are just strings of
+        python code which the target compute node will execute. They're
+        stored in CentralServer.job_queue. To track which node we should
+        send the job to, we have a "ready list," and a "working list;" the
+        former tracks nodes which are ready to compute, and the latter
+        tracks which nodes have had a job sent and still not gotten any
+        response. When a compute node is ready, the server grabs a job from
+        the end of the queue, sends it to that node (with this method), and
+        then moves that entry in the ready list into the waiting list.
+
+        aux_ip directs the ip to which the job should be sent.
+
+        to_send: a message containing the job to be sent. send() converts the
+        string into JSON.
+        """
+
+        # JSON-ify the message to be sent.
+        #to_send.To_Json().encode()
+
+        # Socket options: use ipv4
+        self.socket_cx = socket.socket(
+            socket.AF_INET, socket.SOCK_STREAM
+            )
+        # More options: socket level, reuse socket
+        self.socket_cx.setsockopt(
+            socket.SOL_SOCKET, socket.SO_REUSEADDR, 1
+            )
+        # Establish a connection
+        self.socket_cx.connect(
+            (aux_ip, self.listen_port)
+            )
+        self.socket_cx.send(to_send)
+        self.socket_cx.close()
+
+    def listening(self):
+        self.connection = Connection_Info(socket.gethostbyname(socket.gethostname()))
+        self.socket_con = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # open socket
+        self.socket_con.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket_con.bind((socket.gethostname(), self.connection.listening_port))
+        self.socket_con.listen(5)
+        self.socket_con.setblocking(False)  # opens the non blocking channel
+
+        if self.socket_con:
+            input = [self.socket_con]
+            while True:
+                input_ready, output_ready, errors = select.select(input, [], [])
+
+                for sock in input_ready:
+                    if sock is self.socket_con:
+                        client, address = sock.accept()
+                        input.append(client)
+                    else:
+                        data = sock.recv(self.connection.buffer).decode()
+                        if data:
+                            self.process(data, address[0])
+                        else:
+                            sock.close()
+                            input.remove(sock)
+
+    def process(self, data, ip):
+        data_dict = json.loads(data)
+
+        #Returning data
+        if data_dict["flag"] == "r":
+            print("Raw data --> " + data)
+            print("Returned data --> " + data_dict["body"])
+            self._peer_list[ip] = True
+            print(self._peer_list)
+
+    def send_file(self, file):
+        file_bytes = file_ops.file_to_bytes(file)
+        to_send = Message("f", file_bytes)
+        self.send(to_send, self.server_ip)
+
+    def connect(self, ip):
+        """
+        Connect to a central server using the given ip
+        """
+        # Connect to the central server and tell it how many threads we have
+        join_message = Message('uc', self.avail_threads)
+        self.send_message(join_message)
+
+    def run(self):
+        pass
+
+    def start_server(self):
+        #Server_Run = Process(target=self.run)
+        #Server_Run.start()
+        #Server_List = Process(target=self.listening)
+        #Server_List.start()
+        pass
