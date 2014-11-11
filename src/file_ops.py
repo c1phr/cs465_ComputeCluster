e0fe720015a7a6849491420949ffaf43141e1430
Ryan Batchelder <rb688@nau.edu>
2014-11-11 14:39:22 -0700

    Adding comments to file_ops.py Adding comments to main.py Cleaning up the test files

--- src/file_ops.py
+++ src/file_ops.py
@@ -1,34 +1,32 @@
-from random import randrange
-
 class file_ops(object):
-    '''
-    Takes in the fully-qualified name of a file, opens it and returns a byte array representation
+    """
+    Static: Takes in the fully-qualified name of a file, opens it and returns a byte array representation
     :param file_name:
     :return array:
-    '''
+    """
     @staticmethod
     def file_to_bytes(file_name):
+        # Open the file for reading and read the contents into a byte array
         file_content = open(file_name, 'r').read()
         return file_content
 
-
-    '''
-    Takes in a byte array representation of a file and saves it out as a Python program
-    :param array of file contents:
+    """
+    Static: Takes in a byte array representation of a file and saves it out as a Python program
+    :param byte array of file, optional path to save file to, optional file output name:
     :return name of Python program:
-    '''
+    """
     @staticmethod
     def bytes_to_file(arr, path="", name="in_file.py"):
+        # Add a trailing slash if a path was passed in but the user forgot to include one
         if len(path) > 0 and path[-1] != "/":
             path += "/"
+
+        # Open a new empty file to write the contents to
         new_file = open(path + name, 'w')
+        # Write the byte array to the file, Python will handle the conversion
         new_file.write(arr)
         new_file.close()
         return name
 
-
-#Showing off that this all works
 if __name__ == "__main__":
-    bytes = file_ops.file_to_bytes('file_ops.py')
-    file_ops.bytes_to_file(bytes)
-    print(bytes)
+    print("This module cannot be run alone")
--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ def main():
 
     print("Welcome to Compute Cluster!\n")
     print("Are you a (s)erver, (p)rocessor or (u)ser? (s/p/u)\n")
+    # Figure out what module to start up
     input_identity = input()
 
     #if you're the server
@@ -29,21 +30,28 @@ def main():
     elif input_identity == "p":
         print("Starting Satellite ...\n")
         print("Enter the IP of the server:\n")
+        # Grab the IP of the central server you wish to connect to
         input_serverIP = input()
+        # Create a new Aux processor object and pass it the central server IP
         satellite = AuxiliaryProcessor()
         satellite.connect(input_serverIP)
+        # Sit around and wait for work to do
         satellite.listening()
         print("Waiting for incoming jobs...\n")
 
     else:
         print("Starting user mode...\n")
         print("Enter the IP of the server:\n")
+        # Grab the IP of the central server you wish to connect to
         input_serverIP = input()
         print("Enter the path of the file that you would like to run:\n")
+        # Grab the file that you want to have processed
         input_file = input()
+        # Create a new user object, connect to the central server and send along the file
         user = User()
         user.connect(input_serverIP)
         user.send_file(input_file)
+        # Sit around and wait for a response from your processing
         user.listening()
 
     return
